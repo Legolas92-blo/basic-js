ed7efe756a5496bb1a48395616b71c5a8f49e36e
Asya Beladidze <asya2100@gmail.com>
2023-04-14 21:25:53 +0300

    feat: part of the solution what a season

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	let result = '';
+
+
+	let arr = members.forEach(item => {
+		let itemTrim = item.trim();
+
+		typeof item !== 'string' ? 'false' : result += item.trim().toUpperCase()[0];
+	});
+
+	return result.split('').sort().join('').toUpperCase();
 }
 
 module.exports = {
-  createDreamTeam
+	createDreamTeam
 };
